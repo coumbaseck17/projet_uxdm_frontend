4901781f106eaba8e08bbd4b4448cb2cfe9eded7
Maneva <manevarsld@gmail.com>
2023-11-12 19:11:57 +0100

    subgenres

--- src/components/bar-chart/BarChartV1.vue
+++ src/components/bar-chart/BarChartV1.vue
@@ -3,7 +3,7 @@
     <div>
       <div className="pictogram-chart"></div>
     </div>
-    <div class="genre-details">
+    <div className="genre-details">
       <!-- Contenu des détails du genre -->
     </div>
   </div>
@@ -16,6 +16,7 @@ import pic1000 from '@/data/pic1000.png';
 import pic100 from '@/data/pic100.png';
 import pic10 from '@/data/pic10.png';
 import pic1 from '@/data/pic1.png';
+
 export default {
   data() {
     return {
@@ -25,6 +26,7 @@ export default {
       hauteurTotale: 400,
       innerWidth: 0,
       innerHeight: 0,
+      currentGenre: null, // Track the currently selected genre
     };
   },
 
@@ -54,7 +56,6 @@ export default {
           .attr('width', this.largeurTotale)
           .attr('height', this.hauteurTotale);
 
-
       const yScale = d3.scaleBand()
           .domain(genres)
           .range([0, this.innerHeight])
@@ -62,27 +63,28 @@ export default {
 
       const yAxis = d3.axisLeft(yScale);
 
-
-
       svg.append('g')
           .attr('class', 'y axis')
           .call(yAxis).selectAll('.domain, .tick line')
           .style('stroke', 'transparent');
 
-      svg.selectAll('.pictogram')
+      const pictogramGroups = svg.selectAll('.pictogram')
           .data(genres)
           .enter()
           .append('g')
           .attr('class', 'pictogram')
-          .attr('transform', (genre) => `translate(100, ${yScale(genre)})`).on('mouseover', (event, genre) => {
-        // Logique pour afficher les détails du genre
-        this.showGenreDetails(genre);
-      })
-          .on('mouseout', () => {
-            // Logique pour masquer les détails du genre
-            this.hideGenreDetails();
+          .attr('transform', (genre) => `translate(100, ${yScale(genre)})`)
+          .on('click', (event, genre) => {
+            this.showSubgenres(genre);
           })
-          .selectAll('image')
+          .on('mouseover', (event, genre) => {
+            this.showGenreDetails(genre);
+          })
+          .on('mouseout', () => {
+            this.hideGenreDetails();
+          });
+
+      pictogramGroups.selectAll('image')
           .data((genre) => {
             const count = this.data.genres[genre].details.nombre_artists_total;
             return [
@@ -101,20 +103,120 @@ export default {
           .attr('height', 40)
           .attr('xlink:href', (d) => d);
 
-      // Ajout des labels à côté des pictogrammes
       svg.selectAll('.label')
           .data(genres)
           .enter()
           .append('text')
           .attr('class', 'label')
           .text((genre) => `${genre} `)
-          .attr('x', 10)  // Décalage à gauche des pictogrammes
+          .attr('x', 10)
           .attr('y', (genre) => yScale(genre) + yScale.bandwidth() / 2)
           .attr('dy', '.35em')
           .style('font-size', '14px')
           .style('fill', 'black')
           .style('text-anchor', 'start');
     },
+
+    showSubgenres(genre) {
+      // Update currentGenre to the selected genre
+      this.currentGenre = genre;
+
+      // Remove the existing chart
+      d3.select('.pictogram-chart').selectAll('*').remove();
+
+      // Draw the new chart for subgenres
+      const subgenres = Object.keys(this.data.genres[genre].subgenres).sort((a, b) => {
+        return this.data.genres[genre].subgenres[b].details.nombre_artists_total - this.data.genres[genre].subgenres[a].details.nombre_artists_total;
+      });
+
+      const svg = d3.select('.pictogram-chart').append('svg')
+          .attr('width', this.largeurTotale)
+          .attr('height', this.hauteurTotale);
+
+      const yScale = d3.scaleBand()
+          .domain(subgenres)
+          .range([0, this.innerHeight])
+          .padding(0.1);
+
+      const yAxis = d3.axisLeft(yScale);
+
+      svg.append('g')
+          .attr('class', 'y axis')
+          .call(yAxis).selectAll('.domain, .tick line')
+          .style('stroke', 'transparent');
+
+      const pictogramGroups = svg.selectAll('.pictogram')
+          .data(subgenres)
+          .enter()
+          .append('g')
+          .attr('class', 'pictogram')
+          .attr('transform', (subgenre) => `translate(100, ${yScale(subgenre)})`)
+          .on('mouseover', (event, subgenre) => {
+            // Logique pour afficher les détails du subgenre
+            this.showSubgenreDetails(subgenre);
+          })
+          .on('mouseout', () => {
+            // Logique pour masquer les détails du subgenre
+            this.hideSubgenreDetails();
+          });
+
+      pictogramGroups.selectAll('image')
+          .data((subgenre) => {
+            const count = this.data.genres[genre].subgenres[subgenre].details.nombre_artists_total;
+            return [
+              ...Array(Math.floor(count / 1000)).fill(pic1000),
+              ...Array(Math.floor((count % 1000) / 100)).fill(pic100),
+              ...Array(Math.floor((count % 100) / 10)).fill(pic10),
+              ...Array(count % 10).fill(pic1),
+            ];
+          })
+          .enter()
+          .append('image')
+          .attr('class', 'pictogram')
+          .attr('x', (d, i) => i * 40)
+          .attr('y', 0)
+          .attr('width', 40)
+          .attr('height', 40)
+          .attr('xlink:href', (d) => d);
+
+      svg.selectAll('.label')
+          .data(subgenres)
+          .enter()
+          .append('text')
+          .attr('class', 'label')
+          .text((subgenre) => `${subgenre} `)
+          .attr('x',0)
+          .attr('y', (subgenre) => yScale(subgenre) + yScale.bandwidth() / 2)
+          .attr('dy', '.35em')
+          .style('font-size', '14px')
+          .style('fill', 'black')
+          .style('text-anchor', 'start');
+    },
+
+    showSubgenreDetails(subgenre) {
+      // Logique pour afficher les détails du subgenre
+      // Vous pouvez utiliser la classe 'genre-details' ou toute autre méthode pour afficher les détails
+      // Mise à jour des détails du genre actuel
+      const subgenreDetails = this.data.genres[this.currentGenre].subgenres[subgenre].details;
+
+
+
+
+        // Logique pour afficher les détails du genre
+        const detailsContainer = d3.select('.genre-details');
+        detailsContainer.html(`<p>Nombre de groupe: ${subgenreDetails.nombre_groupes} / Actifs : ${subgenreDetails.nombre_actif_groupes}</p>
+        <p>Nombre de solos: ${subgenreDetails.nombre_solos} / Actifs : ${subgenreDetails.nombre_actif_solos} </p>
+        <p>Nombre autres: ${subgenreDetails.nombre_autres} / Actifs : ${subgenreDetails.nombre_actif_autres} </p>`);
+      },
+
+
+
+    hideSubgenreDetails() {
+      // Logique pour masquer les détails du subgenre
+      // Vous pouvez effacer les détails du subgenre de la classe 'genre-details' ou utiliser une autre méthode
+      d3.select('.genre-details').html('');
+    },
+
     showGenreDetails(genre) {
       // Logique pour afficher les détails du genre
       const detailsContainer = d3.select('.genre-details');
@@ -123,10 +225,11 @@ export default {
         <p>Nombre autres: ${this.data.genres[genre].details.nombre_autres} / Actifs : ${this.data.genres[genre].details.nombre_actif_autres} </p>`);
     },
 
+
     hideGenreDetails() {
       // Logique pour masquer les détails du genre
-      const detailsContainer = d3.select('.genre-details');
-      detailsContainer.html('');
+      // Vous pouvez effacer les détails du genre de la classe 'genre-details' ou utiliser une autre méthode
+      d3.select('.genre-details').html('');
     },
   },
 };
@@ -135,24 +238,39 @@ export default {
 <style scoped>
 .chart-container {
   display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 50vh;
 }
 
 .pictogram-chart {
-
-}
-
-.pictogram {
-  margin-right: 5px;
+  margin-right: 20px;
 }
 
 .genre-details {
-  margin-left: 20px; /* Ajustez la marge selon vos besoins */
+  font-size: 16px;
+}
+
+.axis path,
+.axis line {
+  fill: none;
+  stroke: black;
+  shape-rendering: crispEdges;
+}
+
+.axis text {
+  font-family: 'Arial', sans-serif;
+  font-size: 12px;
 }
 
 .label {
-  /* Ajoutez des styles pour les labels si nécessaire */
+  font-size: 14px;
+  fill: black;
+  cursor: pointer;
+}
+
+.pictogram {
+  cursor: pointer;
+}
+
+.pictogram:hover {
+  filter: brightness(70%);
 }
 </style>
